24b95b4679ae085d4fbe6051d2022552f8488dee
ariadisasaki <52398435+ariadisasaki@users.noreply.github.com>
2026-03-05 21:52:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,39 @@
-const CACHE_NAME = "myloc-cache-v1";
+const CACHE_NAME = "myloc-cache-v2";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/location.js",
-  "/manifest.json"
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./location.js",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(res => res || fetch(event.request))
   );
 });
